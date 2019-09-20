f16d168f34ffd036310b1d42b2b7f602dd7f1d63
Jason Frey <jfrey@redhat.com>
2019-09-20 16:37:40 -0400

    Change trigger to backreference the requesting PR number

--- trigger.sh
+++ trigger.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-body='{
-  "request": {
-    "branch":"build"
+cat << RUBY | ruby -r json
+target =
+  if ENV["TRAVIS_COMMIT_MESSAGE"].to_s =~ /\AMerge pull request (#\d+)/
+    "#{ENV["TRAVIS_REPO_SLUG"]}#{\$1}"
+  else
+    "#{ENV["TRAVIS_REPO_SLUG"]}@#{ENV["TRAVIS_COMMIT"]} (#{ENV["TRAVIS_BRANCH"]})"
+  end
+File.write("/tmp/post.json", {
+  "request" => {
+    "branch"  => "build",
+    "message" => "Deployed site for #{target}"
   }
-}'
+}.to_json)
+RUBY
 
 curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
   -H "Travis-API-Version: 3" \
   -H "Authorization: token $TRAVIS_TOKEN" \
-  -d "$body" \
+  -d @/tmp/post.json \
   https://api.travis-ci.org/repo/ManageIQ%2Fmanageiq.github.io/requests
