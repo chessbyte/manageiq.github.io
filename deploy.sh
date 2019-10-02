1313b19c3b178331c83869b3edd9fa334551ffa2
Jason Frey <jfrey@redhat.com>
2019-10-02 16:04:17 -0400

    Replace GitHub pages deployment with manual deployment

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd dest
+
+echo "www.manageiq.org" > CNAME
+
+git config --global user.name "ManageIQ Bot"
+git config --global user.email "noreply@manageiq.org"
+
+git clone --bare --branch master --depth 5 https://miq-bot:${GITHUB_TOKEN}@github.com/ManageIQ/manageiq.github.io .git
+git config core.bare false
+git add -A
+git commit -m "${TRAVIS_COMMIT_MESSAGE}"
+git push origin master
